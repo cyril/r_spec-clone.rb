aac96ee84b909a4f7604791dbe2590b249ccf82d
Cyril Kato <contact@cyril.email>
2021-06-04 22:01:44 +0200

    fix(Pending): warn instead of notice such expectations

--- lib/r_spec/pending.rb
+++ lib/r_spec/pending.rb
@@ -5,18 +5,18 @@ require "expresenter"
 module RSpec
   # Exception for pending expectations.
   class Pending < ::NotImplementedError
-    # @param description [String] The not implemented expectation description.
+    # @param message [String] The not implemented expectation description.
     #
     # @return [nil] Write a pending expectation to STDOUT.
-    def self.result(description)
+    def self.result(message)
       puts ::Expresenter.call(true).with(
-        actual:   nil,
-        error:    new(description),
-        expected: nil,
-        got:      nil,
-        matcher:  :eql,
-        negate:   false,
-        level:    :MAY,
+        actual:   new(message),
+        error:    nil,
+        expected: self,
+        got:      false,
+        matcher:  :raise_exception,
+        negate:   true,
+        level:    :SHOULD,
         valid:    false
       ).colored_string
     end
